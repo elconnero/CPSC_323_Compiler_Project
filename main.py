3be5277a3564a9b56cc9842dd0da989ac80ad702
elconnero <connerrobbins@gmail.com>
2025-02-12 16:41:25 -0800

    main.py is set up w/ .txt file to use for notes.

---
+++ main.py
@@ -0,0 +1,50 @@
+#Student     : Darren Chen     
+#              Mariah Salgado
+# Team Leader: Conner Robbins
+#Class       : CPSC-323-07 13801
+#Professor   : James S. Choi, Ph.D.
+#Date        : 20250212
+#Due         : 20250302
+# Homework   : 1
+
+keywords = {
+    "integer",
+    "if",
+    "else",
+    "endif",
+    "while",
+    "return",
+    "scan",
+    "print",
+    "function",
+    "scan"
+}
+
+seperator = {
+    ",",
+    "$",
+    "(",
+    ")",
+    ";",
+}
+
+operator = {
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    "<",
+    ">",
+    "=",
+}
+
+comment = {"[*", "*]"}
+
+
+def main():
+    print("Hello world!")
+
+if __name__ ==  "__main__":
+    main()
+
